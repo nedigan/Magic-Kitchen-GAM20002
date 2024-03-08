08a1ccf0327e79ce2ca554285bdef2e417ab8f81
Ethan Hunt <ethan_james_hunt@hotmail.com>
2024-03-08 16:26:01 +1100

    Added Rooms foundations

---
+++ Assets/Prefabs/Doors/Base Door/SceneDoor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class SceneDoor : MonoBehaviour
+{
+    [SerializeField]
+    protected Collider _enteranceCollider;
+    [SerializeField] 
+    protected GameObject _exitPosition;
+
+    public Collider EnteranceCollider => _enteranceCollider;
+    public GameObject ExitPosition => _exitPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
