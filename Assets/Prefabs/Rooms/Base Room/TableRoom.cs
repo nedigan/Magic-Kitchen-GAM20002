e424ed73e4feed07586fd7893bb706f5c4c4b719
Ned Madigan <nedmadigan4@gmail.com>
2024-03-18 19:14:53 +1100

    rooms now air hockey eachother lol

--- Assets/Prefabs/Rooms/Base Room/TableRoom.cs
+++ Assets/Prefabs/Rooms/Base Room/TableRoom.cs
@@ -16,16 +16,24 @@ public class TableRoom : MonoBehaviour
 
     private Vector3 _mousePos;
 
+    private Rigidbody _rb;
+    private Vector3 _targetPos;
+    private float _followSpeed = 10f;
+    private bool _moving = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (_moving)
+        {
+            _rb.velocity = (_targetPos - transform.position) * _followSpeed;
+        }
     }
 
     private Vector3 GetMousePos()
@@ -36,39 +44,46 @@ public class TableRoom : MonoBehaviour
     private void OnMouseDown()
     {
         _mousePos = Input.mousePosition - GetMousePos();
+        _moving = true;
     }
 
     private void OnMouseDrag()
     {
-        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - _mousePos);
-        ClampPosition();
+        _targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition - _mousePos);
+        //ClampPosition();
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnMouseUp()
     {
-        ClampPosition();
+        _moving = false;
+        _rb.velocity = Vector3.zero;
     }
 
-    private void ClampPosition()
-    {
-        transform.position = new Vector3(transform.position.x, 0, transform.position.z);
-
-        if (transform.position.x > _xBounds)
-        {
-            transform.position = new Vector3(_xBounds, transform.position.y, transform.position.z);
-        }
-        else if (transform.position.x < -_xBounds)
-        {
-            transform.position = new Vector3(-_xBounds, transform.position.y, transform.position.z);
-        }
-
-        if (transform.position.z > _zBounds)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, _zBounds);
-        }
-        else if (transform.position.z < -_zBounds)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -_zBounds);
-        }
-    }
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    //ClampPosition();
+    //}
+    //
+    //private void ClampPosition()
+    //{
+    //    transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+    //
+    //    if (transform.position.x > _xBounds)
+    //    {
+    //        transform.position = new Vector3(_xBounds, transform.position.y, transform.position.z);
+    //    }
+    //    else if (transform.position.x < -_xBounds)
+    //    {
+    //        transform.position = new Vector3(-_xBounds, transform.position.y, transform.position.z);
+    //    }
+    //
+    //    if (transform.position.z > _zBounds)
+    //    {
+    //        transform.position = new Vector3(transform.position.x, transform.position.y, _zBounds);
+    //    }
+    //    else if (transform.position.z < -_zBounds)
+    //    {
+    //        transform.position = new Vector3(transform.position.x, transform.position.y, -_zBounds);
+    //    }
+    //}
 }
