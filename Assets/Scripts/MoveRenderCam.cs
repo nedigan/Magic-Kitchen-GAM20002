f438e29e275d6d80550a7500319b93873f40703b
Ned Madigan <nedmadigan4@gmail.com>
2024-04-19 14:38:11 +1000

    attempting camera based room movement - WIP

---
+++ Assets/Scripts/MoveRenderCam.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRenderCam : MonoBehaviour
+{
+    [SerializeField] private Transform _camRoom;
+    [SerializeField] private Transform _physicalRoom;
+    [SerializeField] private Camera _mainCamera;
+
+    private Camera _camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = GetComponent<Camera>();    
+        Debug.Log(_mainCamera.WorldToViewportPoint(_physicalRoom.position));
+        Debug.Log(_camera.WorldToViewportPoint(_camRoom.position));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //_camRoom.position = _camera.ViewportToWorldPoint(_mainCamera.WorldToViewportPoint(_physicalRoom.position));
+        //Debug.Log(_mainCamera.WorldToViewportPoint(_physicalRoom.position));
+        Vector3 pos = (_mainCamera.WorldToViewportPoint(_physicalRoom.position));
+
+        float height = 2f* _camera.orthographicSize;
+        float width = height * _camera.aspect;
+        transform.localPosition = new Vector3((-pos.x * width) + width / 2f, (-pos.y * height) +  height / 2f, transform.localPosition.z);
+    }
+}
